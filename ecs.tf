8f5f063f084df291a235312f4c1b389005a58b96
samuelmaderson <samuel.maderson@gmail.com>
2025-04-22 02:09:53 -0300

    adding dd containers

--- ecs.tf
+++ ecs.tf
@@ -6,113 +6,127 @@ resource "aws_ecs_task_definition" "app_task" {
   family                   = "${var.project_name}-task"
   task_role_arn            = aws_iam_role.ecs_task_role.arn
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  cpu                      = "256"
+  memory                   = "512"
   network_mode             = "awsvpc"
+  pid_mode                 = "task"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
-  volume {
-    name = "cws-instrumentation-volume"
-  }
   container_definitions = jsonencode([
     {
-      "name" : "cws-instrumentation-init",
-      "image" : "datadog/cws-instrumentation:latest",
-      "essential" : false,
-      "user" : "0",
-      "command" : [
+      name = "cws-instrumentation-init"
+      image = "datadog/cws-instrumentation:latest"
+      essential = false
+      user = "0"
+      command = [
         "/cws-instrumentation",
         "setup",
         "--cws-volume-mount",
         "/cws-instrumentation-volume"
-      ],
-      "mountPoints" : [
-        {
-          "sourceVolume" : "cws-instrumentation-volume",
-          "containerPath" : "/cws-instrumentation-volume",
-          "readOnly" : false
-        }
       ]
-    },
-    {
-      name      = "datadog-agent",
-      image     = "datadog/agent:latest",
-      essential = true,
-      environment = [
-        {
-          name  = "DD_API_KEY",
-          value = var.datadog_api_key
-        },
-        {
-          name  = "DD_SITE",
-          value = "datadoghq.com"
-        },
-        {
-          name  = "ECS_FARGATE",
-          value = "true"
-        },
-        {
-          name  = "DD_RUNTIME_SECURITY_CONFIG_ENABLED",
-          value = "true"
-        },
-        {
-          name  = "DD_RUNTIME_SECURITY_CONFIG_EBPFLESS_ENABLED",
-          value = "true"
-        }
-      ],
-      healthCheck = {
-        command = [
-          "CMD-SHELL",
-          "/probe.sh"
-        ],
-        interval    = 30,
-        timeout     = 5,
-        retries     = 2,
-        startPeriod = 60
-      }
-    },
-    {
-      name      = "${var.project_name}-container",
-      image     = "nginx:latest",
-      essential = true,
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-        }
-      ],
       mountPoints = [
         {
-          sourceVolume  = "cws-instrumentation-volume",
-          containerPath = "/cws-instrumentation-volume",
-          readOnly      = true
+          sourceVolume = "cws-instrumentation-volume"
+          containerPath = "/cws-instrumentation-volume"
+          readOnly = false
         }
-      ],
-      entryPoint = [
-        "/cws-instrumentation-volume/cws-instrumentation",
-        "trace",
-        "--",
-        "['nginx','-g','daemon off;']"
-      ],
-      # dependsOn = [
-      #   {
-      #     containerName = "datadog-agent",
-      #     condition     = "HEALTHY"
-      #   },
-      #   {
-      #     containerName = "cws-instrumentation-init",
-      #     condition     = "SUCCESS"
-      #   }
-      # ],
+      ]
       logConfiguration = {
         logDriver = "awslogs"
         options = {
           awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
           awslogs-region        = var.region
-          awslogs-stream-prefix = "nginx"
+          awslogs-stream-prefix = "cws-init"
         }
       }
+    },
+    {
+      name = "datadog-agent"
+      image = "datadog/agent:latest"
+      essential = true
+      environment = [
+        {
+          name = "DD_API_KEY"
+          value = var.datadog_api_key
+        },
+        {
+          name = "DD_SITE"
+          value = "us5.datadoghq.com"
+        },
+        {
+          name = "ECS_FARGATE"
+          value = "true"
+        },
+        {
+          name = "DD_RUNTIME_SECURITY_CONFIG_ENABLED"
+          value = "true"
+        },
+        {
+          name = "DD_RUNTIME_SECURITY_CONFIG_EBPFLESS_ENABLED"
+          value = "true"
+        }
+      ]
+      healthCheck = {
+        command = [
+          "CMD-SHELL",
+          "/probe.sh"
+        ]
+        interval = 30
+        timeout = 5
+        retries = 2
+        startPeriod = 60
+      }
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
+          awslogs-region        = var.region
+          awslogs-stream-prefix = "datadog-agent"
+        }
+      }
+    },
+    {
+      name = "${var.project_name}-container"
+      image = "nginx:latest"
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+          protocol      = "tcp"
+        }
+      ],
+      entry_point = [
+        "/cws-instrumentation-volume/cws-instrumentation",
+        "trace",
+        "--",
+        ["nginx","-g","daemon off;"]
+      ]
+      mountPoints = [
+        {
+          sourceVolume = "cws-instrumentation-volume"
+          containerPath = "/cws-instrumentation-volume"
+          readOnly = true
+        }
+      ]
+      linux_parameters = {
+        capabilities = {
+          add = ["SYS_PTRACE"]
+        }
+      }
+      dependsOn = [
+        {
+          containerName = "datadog-agent"
+          condition = "HEALTHY"
+        },
+        {
+          containerName = "cws-instrumentation-init"
+          condition = "SUCCESS"
+        }
+      ]
     }
   ])
+  volume {
+    name = "cws-instrumentation-volume"
+  }
 }
 
 resource "aws_ecs_service" "app_service" {
